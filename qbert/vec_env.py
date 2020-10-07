9ca2df39d9ab5968f7a5034381483cfcef122a03
Prithviraj Ammanabrolu <rajammanabrolu@gmail.com>
2020-10-07 17:34:33 -0400

    autostart qbert servers and redis caching

--- qbert/vec_env.py
+++ qbert/vec_env.py
@@ -106,8 +106,8 @@ def worker(remote, parent_remote, env, buffer_size, training_type, clear_kg):
 class VecEnv:
 
     def __init__(self, num_envs, env, openie_path, redis_db_path, buffer_size, askbert, training_type, clear_kg):
-        #start_flask()
-        #start_redis(redis_db_path)
+        start_flask()
+        start_redis(redis_db_path)
         self.conn_valid = redis.Redis(host='localhost', port=6379, db=0)
         self.closed = False
         self.total_steps = 0
